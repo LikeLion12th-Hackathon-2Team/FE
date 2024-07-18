fa0c04d8b49aa99c36864c3ceef09c9581f3055f
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-07-18 15:53:18 +0900

    global styles setting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from "./styles/globalStyles";
+import {ThemeProvider} from "styled-components";
+import theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles/>
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+    colors :{
+        mainColor: "#B7E1FF",
+        mainColorDark: "#A0D6BB",
+        white: "#ffffff",
+        darkGray: "#616161",
+        lightGray: "#A9A9A9",
+        darkMint: "#419F70",
+    },
+    backgroundColors: {
+        mainColor: "#B7E1FF",
+        mainColorDark: "#A0D6BB",
+        white: "#ffffff",
+        darkGray: "#616161",
+        lightGray: "#A9A9A9",
+    },
+    mobile: `375px`,
+    tablet: "768px",
+};
+
+export default theme;
