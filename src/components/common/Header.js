a4198cc973c3c8e23892ca52009852ea12fcaf72
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-07-25 16:19:13 +0900

    css: 헤더 css 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,22 +1,27 @@
 import styled from "styled-components";
 import {ChangeLogo} from "../icons/logo";
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
-        <Wrapper>
-            <ChangeLogo>
-            </ChangeLogo>
-        </Wrapper>
+        <Link to={'/soda'}>
+            <Wrapper>
+                <ChangeLogo/>
+            </Wrapper>
+        </Link>
 
     );
 }
 
 export default Header;
 const Wrapper = styled.div`
+    z-index: 1;
+    position: fixed;
+    background: ${({ theme }) => theme.backgroundColors.mainColor};
     display: flex;
     justify-content: center;
-    align-items: center;
     width: 100%;
-    padding: 20px;
+    padding: 10px;
+    border: none;
 `
