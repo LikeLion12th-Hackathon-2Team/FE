818b337aeb8dcc7a7569c2a5559e87cfba9b38e9
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-07-30 13:18:03 +0900

    docs: 코드 리팩토링

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -11,34 +11,6 @@ import GoogleButton from '../../components/login/GoogleButton';
 
 // 로그인 컴포넌트
 const Login = () => {
-    
-    const NAVER_CLIENT_ID = 'g6klrCeO6cJZRv_7jVp1';// 발급받은 클라이언트 아이디
-    const REDIRECT_URI = 'http://localhost:3000/naverLogin'; // Callback URL
-    const STATE = "flase";
-    const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
-  
-    const handleLogin1 = () => {
-      window.location.href = NAVER_AUTH_URL;
-    };
-
-    const Rest_api_key = 'd5bac2ad75ed1005489d0d30a03c6c13';
-    const redirect_uri = 'http://localhost:3000/kakaoLogin'; // Redirect URI
-    // oauth 요청 URL
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
-    
-    const handleLogin2 = () => {
-        
-        window.location.href = kakaoURL;
-    };
-
-    const googleClientId = '1052338939325-qmjfc2tj5b1aakdkd3lv8kvdmh6ov4ie.apps.googleusercontent.com'; // 발급받은 클라이언트 아이디
-    const googleredirect_uri ='http://localhost:3000/googleLogin';
-    const googleURL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${googleClientId}&scope=openid%20profile%20email&redirect_uri=${googleredirect_uri}`;
-
-    const handleLogin3 = () => {
-      window.location.href = googleURL;
-    };
-
   return (
     <Container>
       <LogoBox>
@@ -46,9 +18,9 @@ const Login = () => {
       </LogoBox>
       <p>로그인</p>
       <ButtonContainer>
-        <NaverButton onClick={handleLogin1} />
-        <KakaoButton onClick={handleLogin2} />
-        <GoogleButton onClick={handleLogin3} />
+        <NaverButton/>
+        <KakaoButton/>
+        <GoogleButton/>
       </ButtonContainer>
     </Container>
   );
