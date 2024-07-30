43067830412a2233823b12f17271032eaa109b61
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-07-30 13:18:14 +0900

    docs: base url 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `http://54.180.217.161:8081/swagger-ui/index.html#`,
-    withCredentials: true
+    baseURL: `http://54.180.217.161:8081`,
 });
 
 export default instance;
