ef30e6b30f9ac345c3afda18cc87e3a1ad1aaf30
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-07-31 19:33:14 +0900

    test: api get 받아오는 방법

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import { More } from "../../components/icons/weeklyIcons";
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import instance from "../../api/axios";
+import {getCookie} from "../../auth/cookie";
 
 function Test() {
   const [color, setColor] = useState("");
-
+  const accessToken = getCookie('accessToken');
+  const [testData, setTestData] = useState(null);
   const handleClick = () => {
     if (color === "#96D3FF") {
       setColor("#ff0000");
@@ -13,6 +16,31 @@ function Test() {
     }
   };
 
+  // api 연동 코드 - get 방식
+  const getDonationDate = async ()=>{
+    try{
+      const response = await  instance.get('/api/diary',{
+        headers:{
+          Authorization:` Bearer ${accessToken}`
+        }
+      })
+      console.log('response:', response.data);
+      setTestData(response.data)
+    }catch (e){
+      console.log('error:', e);
+    }
+  }
+
+  // useEffect(()=>{
+  //  처음 렌더링할 때 실행시키고 싶은 함수, 변수 등
+  // },[바뀌는 값 -> 얘가 바뀔때마다 useEffect가 다시 실행됨])
+
+  useEffect(() => {
+    getDonationDate();
+  }, []);
+
+
+
   return (
     <Wrapper>
       <div>
@@ -20,7 +48,18 @@ function Test() {
         <p> 7월의 소다</p>
         <span> 테투리 전체 감싸기 </span>
         <span> 7월의 소다 </span>
-        <ItemBox></ItemBox>
+        <ItemBox>
+         <p>여기에서 부터 api 데이터</p>
+          {/*<div>*/}
+          {/*  {testData.map((item)=>(*/}
+          {/*      <ApiDataBox>*/}
+          {/*        <h1>{item.diaryId}</h1>*/}
+          {/*        <h1>생성시간: {item.createdAt}</h1>*/}
+          {/*        <h1>쥐피티왈: {item.gptComment}</h1>*/}
+          {/*      </ApiDataBox>*/}
+          {/*  ))}*/}
+          {/*</div>*/}
+        </ItemBox>
       </div>
       <TestIcon onClick={handleClick}>
         <More color={color} />
@@ -49,6 +88,9 @@ const Wrapper = styled.div`
   }
 `;
 const ItemBox = styled.div`
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
   margin: auto;
   height: 100vh;
   width: ${({ theme }) => theme.tablet};
@@ -57,6 +99,13 @@ const ItemBox = styled.div`
   @media (max-width: ${({ theme }) => theme.mobile}) {
     width: 324px;
     height: 100vh;
+  }
+  h1{
+    padding: 10px;
+    font-size: 12px;
+    color:black;
+    font-family:"Noto Nastaliq Urdu";
+    
   }
 `;
 
@@ -68,3 +117,7 @@ const TestIcon = styled.div`
     // }
   }
 `;
+
+const ApiDataBox = styled.div`
+  border: 2px solid blue;
+`
